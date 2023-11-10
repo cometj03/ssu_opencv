9d168aa393c34473b2e4407bb5f347ea76151610
cometj03 <harryjung1001@gmail.com>
2023-11-10 09:34:10 +0900

    hw1_1 완료

--- hw1_1.py
+++ hw1_1.py
@@ -1,40 +1,37 @@
 import cv2 as cv
+import sys
 
 """
 체커보드 n by n 구하기
 """
 
 def main():
-    src = cv.imread("checker4.jpg", cv.IMREAD_GRAYSCALE)
+    filename = "board1.jpg" if len(sys.argv) < 2 else sys.argv[1]
+
+    src = cv.imread(filename, cv.IMREAD_GRAYSCALE)
 
     # todo: resize 말고 perspective 트랜스폼 해서 정확도 높이기
-    # 적응형 이진화 해야할지도
-
-    # width = 700
-    # ratio = width / src.shape[1]
-    # inter_flag = cv.INTER_CUBIC if (ratio > 1) else cv.INTER_AREA
-    # src = cv.resize(src, (0, 0), fx=ratio, fy=ratio, interpolation=inter_flag)
+    width = 700
+    ratio = width / src.shape[1]
+    inter_flag = cv.INTER_CUBIC if (ratio > 1) else cv.INTER_AREA
+    src = cv.resize(src, (0, 0), fx=ratio, fy=ratio, interpolation=inter_flag)
 
     blur = cv.GaussianBlur(src, (0, 0), 1.2)
-    _, thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
+    # _, thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
+    thresh = cv.adaptiveThreshold(blur, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 71, 5)
 
     cv.imshow("thresh", thresh)
 
     horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 3))
-    # dilate = cv.dilate(thresh, horizontal_kernel, iterations=2)
     dilate = cv.morphologyEx(thresh, cv.MORPH_DILATE, horizontal_kernel, iterations=1)
     dilate = cv.morphologyEx(dilate, cv.MORPH_CLOSE, horizontal_kernel, iterations=2)
     vertical_kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 5))
-    # dilate = cv.dilate(dilate, vertical_kernel, iterations=2)
     dilate = cv.morphologyEx(dilate, cv.MORPH_DILATE, vertical_kernel, iterations=1)
     dilate = cv.morphologyEx(dilate, cv.MORPH_CLOSE, vertical_kernel, iterations=2)
-
-    cv.imshow("dilate", dilate)
 
     contoured = cv.cvtColor(dilate, cv.COLOR_GRAY2BGR)
     # hierarchy : [Next, Previous, First_Child, Parent]
     cnts, hierarchy = cv.findContours(dilate, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
-    cv.drawContours(contoured, cnts, 0, (0, 255, 0), 2, cv.LINE_8, hierarchy)
 
     # 가장 많은 child의 개수 구하기
     top_level_idx = 0
@@ -43,7 +40,9 @@ def main():
         _child_idx = hierarchy[0, top_level_idx, 2]
         _max = 0
         while _child_idx >= 0:
-            _max += 1
+            if cv.contourArea(cnts[_child_idx]) > 100:
+                _max += 1
+                cv.drawContours(contoured, [cnts[_child_idx]], 0, (0, 255, 0), 2)
             _child_idx = hierarchy[0, _child_idx, 0]
         max_child_count = max(max_child_count, _max)
         top_level_idx = hierarchy[0, top_level_idx, 0]
--- util.py
+++ util.py
@@ -180,7 +180,7 @@ boards = [
     # imread("checker1_full.png"),
     imread("checker2.jpg"),
     # imread("checker3_full_with_pieces.png"),
-    imread("checker4.jpg"),
+    imread("board1.jpg"),
     imread("checker5.jpg"),
     imread("checker6.jpg"),
     imread("checker7_with_pieces.jpg"),
